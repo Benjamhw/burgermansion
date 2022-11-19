5b42849e8ea935744ac4f8eb306348bf6be5d960
benjamhw <benjamin.99@live.no>
2022-11-19 22:45:51 +0100

    Added links to contact items.

--- components/contact.js
+++ components/contact.js
@@ -8,8 +8,42 @@ import {
 } from 'react-icons/ai';
 import { MdMail } from 'react-icons/md';
 import { IoMdPin } from 'react-icons/io';
+import Link from 'next/link';
 
 export default function ContactSection(props) {
+  const contactItems = [
+    {
+      text: '69 28 33 00',
+      icon: <AiFillPhone />,
+      url: 'tel:69283300',
+    },
+    {
+      text: '46 19 34 64',
+      icon: <AiFillMobile />,
+      url: 'tel:46193464',
+    },
+    {
+      text: 'vinus.kebab@icloud.com',
+      icon: <MdMail />,
+      url: 'mailto:vinus.kebab@icloud.com',
+    },
+    {
+      text: 'Vinus, Mosseveien 16, 1640 Råde',
+      icon: <IoMdPin />,
+      url: 'http://maps.google.com/?q=Vinus, Mosseveien 16, 1640 Råde',
+    },
+    {
+      text: '@vinuskebab',
+      icon: <AiFillInstagram />,
+      url: 'https://www.instagram.com/vinuskebab/',
+    },
+    {
+      text: 'Vinus Kebab As',
+      icon: <AiFillFacebook />,
+      url: 'https://www.facebook.com/people/Vinus-Kebab-As/100054486819161/',
+    },
+  ];
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.contact}>
@@ -25,18 +59,14 @@ export default function ContactSection(props) {
         <div>
           <h1>Kontakt oss</h1>
           <div className={styles.iconList}>
-            <AiFillPhone />
-            <p>69 28 33 00</p>
-            <AiFillMobile />
-            <p>46 19 34 64</p>
-            <MdMail />
-            <p>vinus.kebab@icloud.com</p>
-            <IoMdPin />
-            <p>Vinus, Mosseveien 16, 1640 Råde</p>
-            <AiFillInstagram />
-            <p>@vinuskebab</p>
-            <AiFillFacebook />
-            <p>Vinus Kebab As</p>
+            {contactItems.map(x => (
+              <Link href={x.url} key={x.text}>
+                <div className={styles.contactItem}>
+                  {x.icon}
+                  <p>{x.text}</p>
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
